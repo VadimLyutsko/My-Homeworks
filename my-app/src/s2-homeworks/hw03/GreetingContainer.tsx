f3f1bb85acba712f4af538d657cbcaa2bb791ef8
vadim <vadim_lyutsko@mail.ru>
2022-10-17 20:23:28 +0300

    The third homework assignment

--- my-app/src/s2-homeworks/hw03/GreetingContainer.tsx
+++ my-app/src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,53 +1,69 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
-import Greeting from './Greeting'
-import { UserType } from './HW3'
+import React, {ChangeEvent, Dispatch, KeyboardEvent, useState} from 'react';
+import Greeting from './Greeting';
+import {UserType} from './HW3';
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[] // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
-export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
+export const pureAddUser = (name: string, setError: Dispatch<string>, setName: Dispatch<string>, addUserCallback: (name: string) => void) => {
     // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
-}
+    name = name.trim();  // Правильно ли я отсеял???
+    if (name === '') {
+        return setError('Ошибка! Введите имя!');
+    } else {
+        addUserCallback(name);
+        setName('');
+    }
+};
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
-}
+export const pureOnBlur = (name: string, setError: Dispatch<string>) => { // если имя пустое - показать ошибку
+    // if (name === '') {
+    //     return setError('Ошибка! Введите имя!');
+    // }
+    name = name.trim();
+    !name && setError('Ошибка! Введите имя!');
+};
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
-}
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: ()=>void) => { // если нажата кнопка Enter - добавить
+    e.key === 'Enter' && addUser()
+    // if(e.key==='Enter'){
+    //     addUser()
+    // }
+};
 
 // более простой и понятный для новичков
 // function GreetingContainer(props: GreetingPropsType) {
 
 // более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
     // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>(''); // need to fix any
+    const [error, setError] = useState<string>(''); // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setName(e.currentTarget.value); // need to fix
 
-        error && setError('')
-    }
+        error && setError('');
+    };
     const addUser = () => {
-        pureAddUser(name, setError, setName, addUserCallback)
-    }
+        pureAddUser(name, setError, setName, addUserCallback);
+    };
 
     const onBlur = () => {
-        pureOnBlur(name, setError)
-    }
+        pureOnBlur(name, setError);
+    };
 
-    const onEnter = (e: any) => {
-        pureOnEnter(e, addUser)
-    }
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        pureOnEnter(e, addUser);
+    };
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+    const totalUsers = users.length; // need to fix
+    const lastUserName = name; // need to fix
 
     return (
         <Greeting
@@ -60,7 +76,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
             totalUsers={totalUsers}
             lastUserName={lastUserName}
         />
-    )
-}
+    );
+};
 
-export default GreetingContainer
+export default GreetingContainer;
--- my-app/src/s2-homeworks/hw03/HW3.tsx
+++ my-app/src/s2-homeworks/hw03/HW3.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {Dispatch, useState} from 'react';
 import { v1 } from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
@@ -19,20 +19,22 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: number // need to fix any
+    name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
+export const pureAddUserCallback = (name: string, setUsers: Dispatch<UserType[]>, users: UserType[]) => { // need to fix any
     const user = { // need to fix
+        _id:1,
+        name:name,
     }
     setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
     }
 
