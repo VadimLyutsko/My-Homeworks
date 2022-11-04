745c16a1d92d7f781a3e8cee8d6a8d6bea1b168a
vadim <vadim_lyutsko@mail.ru>
2022-11-04 10:25:24 +0300

    Fixed bugs

--- my-app/src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ my-app/src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,10 +18,10 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+        + (disabled
+                ? ' '+s.disabled
+                : xType === 'red'
+                    ? ' '+s.red: ' '+s.secondary)
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
--- my-app/src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ my-app/src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -28,6 +28,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
+       return  onChangeChecked&& onChangeChecked(e.currentTarget.checked)
 
     }
 
