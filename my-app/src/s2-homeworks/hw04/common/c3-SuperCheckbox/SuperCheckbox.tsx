45731348202b42a9d05c461be0a4c76dc43a841b
vadim <vadim_lyutsko@mail.ru>
2022-11-04 11:54:50 +0300

    Fixed bugs

--- my-app/src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ my-app/src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,8 +27,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange && onChange(e)
-        onChangeChecked&&onChangeChecked(e.currentTarget.checked)
+        onChange?.(e)
+        onChangeChecked?.(e.currentTarget.checked)
     }
 
     const finalInputClassName = s.checkbox
