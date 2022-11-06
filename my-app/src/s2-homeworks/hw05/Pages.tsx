c4875c140287a14250fe280c2f7b8843523c1837
vadim <vadim_lyutsko@mail.ru>
2022-11-06 23:55:54 +0300

    Add Router-dom

--- my-app/src/s2-homeworks/hw05/Pages.tsx
+++ my-app/src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate, NavLink} from 'react-router-dom';
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -14,19 +14,21 @@ export const PATH = {
 function Pages() {
     return (
         <div>
+
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                <Route path ={'/pre-junior/'} element={<PreJunior/>}/>
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path = {'/pre-junior'} element={<PreJunior/>}/>
+                <Route path = {'/junior'} element={<Junior/>}/>
+                <Route path = {'/junior-plus'} element={<JuniorPlus/>}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path ={'/*'} element={<Error404 />}></Route>
             </Routes>
         </div>
     )
--- my-app/src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ my-app/src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,8 +1,8 @@
-import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
-import s from './Sidebar.module.css'
-import {PATH} from '../Pages'
-import closeIcon from './closeOutline.svg'
+import React, {FC} from 'react';
+import {NavLink} from 'react-router-dom';
+import s from './Sidebar.module.css';
+import {PATH} from '../Pages';
+import closeIcon from './closeOutline.svg';
 
 type PropsType = {
     open: boolean
@@ -11,7 +11,7 @@ type PropsType = {
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
+        + (open ? ' ' + s.open : '');
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={(params) => params.isActive ? s.active : ''} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={(params) => params.isActive ? s.active : ''} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,12 +47,12 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={(params) => params.isActive ? s.active : ''} // делает студент
                     >
                         Junior Plus
                     </NavLink>
                 </nav>
             </aside>
         </>
-    )
-}
+    );
+};
