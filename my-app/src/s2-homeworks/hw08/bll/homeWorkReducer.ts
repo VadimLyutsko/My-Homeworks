f2836b8c65bfd898570507ea8115154425374a3d
vadim <vadim_lyutsko@mail.ru>
2022-11-08 19:47:56 +0300

    Add Homework #8

--- my-app/src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ my-app/src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,20 @@
-import {UserType} from '../HW8'
+import {UserType} from '../HW8';
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            return action.payload === 'up' ? state.sort((a, b) => a.name > b.name ? 1 : -1) : state.sort((a, b) => a.name > b.name ? -1 : 1);
+
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(item => item.age >= 18);   // need to fix
         }
         default:
-            return state
+            return state;
     }
-}
+};
